00a7edafa9428281879311c61c17b48d0ec2638d
florrosaurus <loomansflor@gmail.com>
2025-01-20 10:44:28 +0100

    GUI ontwerp
    Het main design van de GUI is ontworpen.

---
+++ client/assets/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// debug: controleer of de script runt of niet
+console.log("Script is gelinkt en runt");
+
+// placeholder
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("welkom bij mijn gipproject");
+});
